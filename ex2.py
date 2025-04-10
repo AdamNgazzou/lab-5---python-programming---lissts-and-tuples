42664def9336f5a1376495c8990058b0b882ce7b
AdamNgazzou <adem.ng123@gmail.com>
2025-04-10 13:47:51 +0100

    ex2 and ex3

--- ex2.py
+++ ex2.py
@@ -2,16 +2,32 @@
 
 # 1. Créer une liste de tuples contenant les coordonnées des villes
 # villes = [("Paris", (lat1, lon1)), ("New York", (lat2, lon2)), ...]
-
+villes = []
+for i in range(2):
+    nom_ville = input(f"Entrez le nom de la ville {i + 1} : ")
+    latitude = float(input(f"Entrez la latitude de {nom_ville} : "))
+    longitude = float(input(f"Entrez la longitude de {nom_ville} : "))
+    villes.append((nom_ville, (latitude, longitude)))
 # 2. Afficher chaque ville avec ses coordonnées
 # for ville in villes:
     # ..................
-
+for ville in villes:
+    nom, coord = ville 
+    latitude, longitude = coord
+    print(f"Ville : {nom}, Latitude : {latitude}, Longitude : {longitude}")
 # 3. Afficher uniquement les latitudes
 # for ville in villes:
     # ..................
-
+for ville in villes:
+    nom, coord = ville 
+    latitude, longitude = coord
+    print(f"Latitude de {nom} : {latitude}")
 # 4. Calculer et afficher la latitude moyenne
 # latitudes = [...]
+latitudes = [coord[1][0] for coord in villes]
+print(f"latitudes pour tous les villes : {latitudes}")
+
 # moyenne = ...
+moyenne = sum(latitudes) / len(latitudes)
 # print(...)
+print(f"Latitude moyenne : {moyenne}")
--- ex3.py
+++ ex3.py
@@ -1,3 +1,34 @@
 # Exercice 3 : Correction et Optimisation avec Listes et Tuples
+# Example data
+data = [
+    ("Alice", [14, 16, 18]),
+    ("Bob", [12, 9, 10]),
+    ("Charlie", [19, 20, 17]),
+    ("Diana", [8, 7, 10])
+]
+# 1. Display the average score of each student where the average is greater than or equal to 15
+# 2 figuring out the maximum average score among all students
+max = 0 
+count = 0 
+students = []
+for dot in data :
+    test = True
+    average = 0 
+    name, scores = dot
+    for score in scores :
+        # 3 : If the score is less than 10, set test to False and count the number of students that have at least one score less than 10 
+        if score <10 and test == True: 
+            test = False
+            count += 1
+        average += score
+    # ...........
+    if average >= 15 :
+        if(max < average) :
+            max = average
+        students.append((name , round(average/len(scores))))
 
-# ...........
+
+#  Display the highest individual score among all students    
+print(f"Students with an average score greater than or equal to 15:{students}")
+print(f"Count of students that have at least one score under 10 : {count}")
+print(f"Highest student score : {max}")
